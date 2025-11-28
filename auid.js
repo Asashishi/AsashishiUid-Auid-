f11f1007531e0fdda20190d572ad9013c076f992
Asashishi_Nyan! <assassinjpwh@outlook.com>
2025-11-28 09:09:56 +0800

    Fix random number generation in #gTimeOffsetB36

--- auid.js
+++ auid.js
@@ -17,7 +17,7 @@ class Auid {
     if (typeof process !== "undefined" && process.hrtime)
       return process.hrtime()[1].toString(36).toUpperCase().padStart(6, "0");
     const \u{b5}s = (performance.now() | 0) % 1000;
-    const rn = Math.random() * 1e9 - \u{b5}s | 0;
+    const rn = (Math.random() * (1e9 - \u{b5}s)) | 0;
     return (\u{b5}s + rn).toString(36).toUpperCase().padStart(6, "0");
   }
   static gen8() {
